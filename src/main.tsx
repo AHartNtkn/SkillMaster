3c0d014c67a104b346334daa14ed4ded1541a8a2
Anthony Hart <soobtoob@gmail.com>
2025-05-22 14:15:35 -0400

    Add Redux store and SaveManager init

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,34 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { I18nProvider } from './i18n.tsx';
-import { ThemeProvider } from './theme.tsx';
-import 'mathjax/es5/tex-mml-chtml.js';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { promises as fs } from 'fs'
+import path from 'path'
+import './index.css'
+import App from './App.tsx'
+import { I18nProvider } from './i18n.tsx'
+import { ThemeProvider } from './theme.tsx'
+import 'mathjax/es5/tex-mml-chtml.js'
+import { SaveManager } from './saveManager'
+import { createAppStore } from './store'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <I18nProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </I18nProvider>
-  </StrictMode>,
-);
+async function start() {
+  const prefsRaw = await fs.readFile(path.join('save', 'prefs.json'), 'utf8')
+  const prefs = JSON.parse(prefsRaw) as { profile: string }
+  const manager = new SaveManager(prefs.profile)
+  await manager.load()
+  const store = createAppStore(manager)
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <Provider store={store}>
+        <I18nProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </I18nProvider>
+      </Provider>
+    </StrictMode>,
+  )
+}
+
+start()
