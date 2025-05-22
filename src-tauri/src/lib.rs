c4778445bd9b8eb4a2f8eec2b8b3f4d96116e205
AHartNtkn <soobtoob@gmail.com>
2025-05-22 19:42:04 -0400

    Fix tauri build.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 use std::fs::{self, File};
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
-use tauri::api::shell;
+use std::io::Write;
+use std::path::PathBuf;
+use tauri::{AppHandle, Runtime, Window};
+use tauri_plugin_shell::ShellExt;
 
 use tauri::command;
 
@@ -44,9 +45,15 @@ fn write_file(path: String, data: String) -> Result<(), String> {
 }
 
 #[command]
-fn open_folder(path: String, window: tauri::Window) -> Result<(), String> {
-  let p = full_path(&path);
-  shell::open(&window.shell_scope(), p, None).map_err(|e| e.to_string())
+#[allow(deprecated)]
+async fn open_file<R: Runtime>(app: AppHandle<R>, _window: Window<R>, path: String) -> Result<(), String> {
+  let p = PathBuf::from(&path);
+  if !p.exists() {
+    return Err(format!("File not found: {}", path));
+  }
+
+  let shell = app.shell();
+  shell.open(path, None).map_err(|e| e.to_string())
 }
 
 #[command]
@@ -89,7 +96,8 @@ fn get_schedule() -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![read_file, write_file, log_error, log_debug, get_schedule, open_folder])
+    .plugin(tauri_plugin_shell::init())
+    .invoke_handler(tauri::generate_handler![read_file, write_file, log_error, log_debug, get_schedule, open_file])
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
